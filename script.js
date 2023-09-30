52b1e242d463f5a880a149604d806388ececaa47
Faisal <ifaisalmalik99@gmail.com>
2023-09-30 10:27:43 +0530

    add white color in the list of colors

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+
+
+const btn = document.getElementById("btn").addEventListener("click", changeColors);
+
 var index = 0;
 function changeColors(){
-    var colors = ["red", "blue", "green","yellow", "orange"]    
+    var colors = ["red", "blue", "green","yellow", "orange", "white"]
     document.getElementsByTagName('body')[0].style.background = colors[index++]
 
     if(index > colors.length - 1) {
         index = 0
     }
-    
+
 }
